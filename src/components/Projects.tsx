faff5531b1ef000d017d83df2485d6c5e84dc19f
Jason Javandel <jasonjavandel@protonmail.com>
2025-06-24 12:50:18 -0700

    Remove legacy project

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -22,20 +22,6 @@ function Projects() {
 
         <div className="md:w-1/2 h-auto relative bg-[#f5f0ff] border-2 border-[#8572b1] rounded-xl shadow-[0_0.75em_0_0_#d2c4f9] transition-all duration-150 hover:translate-y-1 hover:shadow-[0_0.5em_0_0_#d2c4f9] active:translate-y-3 active:shadow-none overflow-hidden">
           <a
-            href="https://javandel.net/kate"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="block"
-          >
-            <Card
-              imagePath={`${import.meta.env.BASE_URL}images/mother.png`}
-              title="My Mother's Website"
-              description="My mother has been a web publisher for a number of years, however, she didn't have time to work on it herself, so she used an HTML template. I re-wrote her portfolio webpage in pure HTML and CSS in an afternoon, speeding up the page remarkably while also giving it a fresh coat of paint."
-            />
-          </a>
-        </div>
-        <div className="md:w-1/2 h-auto relative bg-[#f5f0ff] border-2 border-[#8572b1] rounded-xl shadow-[0_0.75em_0_0_#d2c4f9] transition-all duration-150 hover:translate-y-1 hover:shadow-[0_0.5em_0_0_#d2c4f9] active:translate-y-3 active:shadow-none overflow-hidden">
-          <a
             href="https://github.com/cherryyeti/csc317-final"
             target="_blank"
             rel="noopener noreferrer"
